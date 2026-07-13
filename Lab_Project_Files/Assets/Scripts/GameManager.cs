d0b43d707e51eee510468250633d0deb7708a7e0
R4v3nB14ck <scorpionlol543@gmail.com>
2026-07-13 14:40:55 -0400

    More Fixes

--- Lab_Project_Files/Assets/Scripts/GameManager.cs
+++ Lab_Project_Files/Assets/Scripts/GameManager.cs
@@ -1,30 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.EnhancedTouch;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject MobileControls;
-    bool lastMobileState;
     public InputActionReference PauseAction;
     public GameObject PauseMenu;
 
     private void Start()
     {
-        PauseMenu.SetActive(false);
+        Pause();
     }
 
     public void Pause()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         PauseMenu.SetActive(true);
     }
 
     public void UnPause()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         PauseMenu.SetActive(false);
     }
 
@@ -33,11 +31,6 @@ public class GameManager : MonoBehaviour
         //Pause Menu
         if (PauseAction.action.WasPressedThisFrame()) if (PauseMenu.activeSelf) Pause(); else UnPause();
 
-        bool mobile = Application.isMobilePlatform;
-        if (mobile != lastMobileState)
-        {
-            lastMobileState = mobile;
-            MobileControls.SetActive(mobile);
-        }
+        MobileControls.SetActive(Application.isMobilePlatform);
     }
 }
---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "R4v3nB14ck-Lab_Project_Files-0.1";
+const contentToCache = [
+    "Build/Lab_Project.loader.js",
+    "Build/Lab_Project.framework.js.unityweb",
+    "Build/Lab_Project.data.unityweb",
+    "Build/Lab_Project.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
